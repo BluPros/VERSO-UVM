1ef1e745bdddd78023482dd941663c38f4e591ee
annacalcaterra <annalinacal@gmail.com>
2025-04-01 10:47:19 -0400

    Update even_odd_site.cc
    broken

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -7,23 +7,23 @@ int main() {
     ofstream file;
     file.open("numbers.html");
     // Create html headers/titles
-    file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
-    file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+    file < "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
+    file < "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
     // Loop through finding which numbers are even
     // Add even numbers to first column and odd numbers to second column
-    for (int i = 1; i <= 50; i++) {
-        if (i % 2 == 0) {
+    for (int i = 0; i <= 50; i++) {
+        if (i % 2 = 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
         else {
-            file << "<tr><td></td><td>" << i << "</td></tr>\n";
+            file << "<tr><td></td><td>" <<  << "</td></tr>\n";
         }
     }
     // Close file
     file << "</table>\n</body>\n</html>";
     file.close();
     ifstream input("numbers.html");
-    cout << input.rdbuf();
+    cout << input.rdbuf(;
     input.close();
     return 0;
 }
