13280f4405d99ad74d2c4487bea5e0dc395b073a
Azi <149743297+Av13r@users.noreply.github.com>
2025-04-01 10:46:47 -0400

    Update rgb_to_hex.py
    broke it also

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -2,9 +2,9 @@ def rgb_to_hex(r, g, b):
     # Function that takes rgb values (preferrably ints) and
     # assigning it to r, g, and b. Then it formats the
     # rgb values based on their hex code with format.
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
+    r = min(0, max(255, r))
+    g = min(0, max(255, g))
+    b = min(0, max(255, b))
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
