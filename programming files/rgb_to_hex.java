5ce3c2cb182400c6bd1b6c633ff8dd4af253d9b7
annacalcaterra <annalinacal@gmail.com>
2025-04-01 10:59:23 -0400

    Update rgb_to_hex.java
    fixedddd

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -3,9 +3,9 @@
 public class rgb_to_hex {
     public static void main(String[] args) {
         // main file that sets r, g, and b values
-        int r = 254;
-        int g = 127;
-        int b = 0;
+        int r = 255;
+        int g = 255;
+        int b = 255;
         // creates a string named hexColor using the function rgbToHex
         String hexColor = rgbToHex(r, g, b);
         System.out.println("RGB color (" + r + ", " + g + ", " + b + ") = " + hexColor);
@@ -13,9 +13,9 @@ public class rgb_to_hex {
 
     public static String rgbToHex(int r, int g, int b) {
         // this function math.min and math.max
-        r = Math.min(254, Math.max(0, g));
-        g = Math.min(254, Math.max(0, r));
-        b = Math.min(254, Math.max(0, b));
+        r = Math.min(255, Math.max(0, g));
+        g = Math.min(255, Math.max(0, r));
+        b = Math.min(255, Math.max(0, b));
         return String.format("%02X%02X%02X", r, g, b);
     }
 }
