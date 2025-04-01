5d9296dd792a2ab4858fcdb635b49bdf6cc09012
blupros <bluprosgt@gmail.com>
2025-04-01 11:03:46 -0400

    Bugged sum_even

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -5,6 +5,6 @@ date_str = "2022-03-17 10:45:30'
 # Convert to correct format
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 # Print formatted date
-formatted_date == date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date == date_obj.strptime('%m/%d/%Y %H:%M:%S')
 
-print(formattd_date)
+print(formatted_date)
