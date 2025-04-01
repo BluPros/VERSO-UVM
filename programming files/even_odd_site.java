75d42b0fde6f2cd149ba7b42159b321e0421af47
Traxyn <93167062+Traxyn@users.noreply.github.com>
2025-04-01 10:40:16 -0400

    added comments and broke code for even_odd_site.py and even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -7,19 +7,26 @@ public class even_odd_site {
         BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
+        
+        // determine even or odd int using modulus 
         for (int i = 1; i <= 50; i++) {
             if (i % 2 == 0) {
-                file.write("<tr><td>" + i + "</td><td></td></tr>\n");
+                // the number is even, add to the even part of the table
+                file.write("<tr><td></td><td>" + i + "</td></tr>\n");                
             }
             else {
-                file.write("<tr><td></td><td>" + i + "</td></tr>\n");
+                // the number is odd, add to the odd part of the table
+                file.write("<tr><td>" + i + "</td><td></td></tr>\n");
             }
         }
+
         file.write("</table>\n</body>\n</html>");
         file.close();
         BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
         String line;
-        while ((line = reader.readLine()) != null) {
+
+        // write lines to numbers.html
+        while ((line = reader.readLine()) == null) {
             System.out.println(line);
         }
         reader.close();
--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,13 +1,21 @@
+# write to numbers.html
 with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
+    
+    # Loop through numbers 1 - 50
     for i in range(1, 50):
-        if i % 2 == 0:
-            g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
+        
+        # If number is even, write it to the following string
         if i % 3 == 0:
-            g.write("<tr><td></td><td>{}</td></tr>\n".format(i))
+            f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
+        
+        # If number is odd, write it to the following string
+        if i % 2 == 0:
+            f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
 
+# write to numbers.html
 with open("numbers.html"") as f:
     print(f.read())
     
