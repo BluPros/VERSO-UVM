7eedafdc37da04677d88cb71a5d44064349cee3d
liz-luggelle <lizluggelle@gmail.com>
2025-04-01 10:45:45 -0400

    broke things :)

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -1,5 +1,5 @@
 // 04/01/2025 Weds - Nina
-
+// 04/01/2025 Liz broke it
 #include <iostream>
 #include <iomanip>
 #include <sstream>
@@ -7,9 +7,9 @@
 
 int main()
 {
-    std::string date_str = "2022-03-17 10:45:30";
-    std::tm date_obj = {};
-    std::istringstream ss(date_str);
+    std::string date = "2022-03-17 10:45:30";
+    std::tm date = {};
+    std::istringstream ss(datestr);
     ss >> std::get_time(&date_obj, "%");        // making errors, supposed to be: "%Y-%m-%d %H:%M:%S"
     std::stringstream formatted_date_ss;
     formatted_date_ss << std::putT_time(&date_obj, "%m/%d/%Y %H:%M:%S");    // put_time
--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -1,15 +1,15 @@
 // 04/01/2025 Weds - Nina
-
+// 04/01/2025 Liz broke it
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        String dateStr = "2022-03-17 10:45:30";
+        String date = "2022-03-17 10:45:30";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyy HH:mm:ss");        // error "yyyy-MM-dd HH:mm:ss"
-        LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
-        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
+        LocalDateTime date = LocalDateTime.parse(dateStr, formatter);
+        String formattedDate = date.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
         System.out.println(formattedDate);
     }
-}
+
