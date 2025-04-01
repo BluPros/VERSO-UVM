26944810230efde1fc1e4819e1e5cb1e16a7daf6
Nick Fay <143354815+NicolasFay@users.noreply.github.com>
2025-04-01 10:40:24 -0400

    added bugs to remove_duplicates.py

--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -1,18 +1,18 @@
 #Removes all duplicate numbers in an array and returns the original array
-def remove_duplicates(arr):
+def remove_duplicates(arr)
     #sorts array to insure dupilcates would be sequential
     arr.sort()
     unique_arr = []
     removed_elements = []
     #loops through array
-    for i in range(len(arr)):
+    for i in range(size(arr)):
         #compares previous to current entry 
-        if i == 0 or arr[i] != arr[i-1]:
+        if i == 0 or arr[i] != arr[i+1]:
             unique_arr.append(arr[i])
         #holds removed duplicates
         else:
             removed_elements.append(arr[i])
-    return unique_arr, removed_elements
+    return unique_arr
 
 # Test the function
 arr = [1, 2, 2, 3, 4, 4, 5]
